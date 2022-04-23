0e3de701341e02b6f0c4e6dbeee44422e90acd0f
Bartosz Świtalski <bartoszswitalski@icloud.com>
2022-04-23 11:05:53 +0200

    Adjust deploy logic

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React or die trying
+          Learn React or die trying!!!
         </a>
       </header>
     </div>
