bda23655ca539268643d4a38bc4faf59344de82c
Bartosz Świtalski <bartoszswitalski@icloud.com>
2022-06-08 20:32:46 +0200

    Adjust landing page

--- src/components/landingPage/statsSection/StatsSection.tsx
+++ src/components/landingPage/statsSection/StatsSection.tsx
@@ -1,7 +1,42 @@
 import {Box, Flex, Heading, SimpleGrid, Spacer, Text } from "@chakra-ui/react";
 import { Parallax } from 'react-scroll-parallax';
+import { useScrollPercentage } from "react-scroll-percentage";
+import {useEffect} from "react";
+import {useSetState} from "../../../hooks/useSetState";
+
+const LEAGUES = 13;
+const COUNTRIES = 3;
+const GRADES = 748;
+
+interface State {
+  leaguesCount: number;
+  countriesCount: number;
+  gradesCount: number;
+}
 
 export const StatsSection = () => {
+  const [ref, percentage] = useScrollPercentage({
+    /* Optional options */
+    threshold: 0,
+  });
+
+  const [state, setState] = useSetState({
+    leaguesCount: 0,
+    countriesCount: 0,
+    gradesCount: 0,
+  } as State);
+
+  useEffect(() => {
+    let factor: number = percentage;
+    if (percentage > 0.5) {
+      factor = 0.5;
+    }
+    setState({
+      leaguesCount: Math.round(factor * 2 * LEAGUES),
+      countriesCount: Math.round(factor * 2 * COUNTRIES),
+      gradesCount: Math.round(factor * 2 * GRADES),
+    } as State);
+  }, [percentage]);
 
   return (
     <>
@@ -12,6 +47,7 @@ export const StatsSection = () => {
         direction={'column'}
         align={'center'}
         backgroundColor={'gray.400'}
+        ref={ref}
       >
         <Spacer />
         <Parallax speed={-10}>
@@ -23,7 +59,7 @@ export const StatsSection = () => {
         <SimpleGrid columns={[1, 3]} spacing={'100'}>
           <Parallax speed={-15}>
           <Box>
-            <Heading fontSize={'8xl'}>7</Heading>
+            <Heading fontSize={'8xl'}>{state.leaguesCount}</Heading>
             <Text fontSize={'4xl'} fontWeight={'light'}>
               Leagues.
             </Text>
@@ -32,7 +68,7 @@ export const StatsSection = () => {
 
           <Parallax speed={-15}>
             <Box>
-              <Heading fontSize={'8xl'}>3</Heading>
+              <Heading fontSize={'8xl'}>{state.countriesCount}</Heading>
               <Text fontSize={'4xl'} fontWeight={'light'}>
                 Countries.
               </Text>
@@ -41,7 +77,7 @@ export const StatsSection = () => {
 
           <Parallax speed={-15}>
             <Box>
-              <Heading fontSize={'8xl'}>748</Heading>
+              <Heading fontSize={'8xl'}>{state.gradesCount}</Heading>
               <Text fontSize={'4xl'} fontWeight={'light'}>
                 Matches graded.
               </Text>
