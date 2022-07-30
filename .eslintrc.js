aa692e84f02d31dc2b0cbfa07ea236311f37d2e8
Bartosz Świtalski <45360754+bartoszswitalski@users.noreply.github.com>
2022-07-30 16:27:31 +0200

    Adjust landing page (#21)

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,11 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,20 +1,5 @@
 import { Hero } from 'components/landingPage/hero/Hero';
-import { StatsSection } from 'components/landingPage/statsSection/StatsSection';
-import { PagesSection } from 'components/landingPage/pagesSection/PagesSection';
-import { useMobile } from 'hooks/useMobile';
 
 export const LandingPage = () => {
-  const { isMobile } = useMobile();
-
-  return (
-    <>
-      <Hero />
-      {!isMobile && (
-        <>
-          <StatsSection />
-          <PagesSection />
-        </>
-      )}
-    </>
-  );
+  return <Hero />;
 };
